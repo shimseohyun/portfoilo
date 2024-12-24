1ea23c663a122cc4423e03e9bf29905edaf103af
shimseohyun <shim020924@gmail.com>
2024-12-25 07:53:43 +0900

    Server Setting CORS

--- server/api/index.js
+++ server/api/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://localhost:5173", "https://localhost:5173"],
+    origin: ["http://localhost:5173", "https://localhost:5173"],
   })
 );
 
