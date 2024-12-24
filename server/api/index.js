6bba50e1aa03cd83edb9a73a1282bf99138dd162
shimseohyun <shim020924@gmail.com>
2024-12-25 08:01:35 +0900

    NOTION API

--- server/api/index.js
+++ server/api/index.js
@@ -9,8 +9,44 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+const fetchNotionData = async (notionToken, notionDatabaseID) => {
+  const url = `https://api.notion.com/v1/databases/${notionDatabaseID}/query`;
+
+  try {
+    const response = await axios.post(
+      url,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${notionToken}`,
+          "Notion-Version": "2022-06-28", // API 버전 설정
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data from Notion:", error);
+    throw new Error("Error fetching data from Notion");
+  }
+};
+
+app.get("/api/notion/databases", async (req, res) => {
+  const { notionToken, notionDatabaseID } = req.query;
+
+  if (!notionToken || !notionDatabaseID) {
+    return res
+      .status(400)
+      .json({ message: "notionToken and notionDatabaseID are required" });
+  }
+
+  try {
+    const notionData = await fetchNotionData();
+    res.json(notionData);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch data from Notion" });
+  }
 });
 
 export default app;
