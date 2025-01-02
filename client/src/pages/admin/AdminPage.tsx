a35b2cceadb41bc8154639db683c1d3846ddeb02
shimseohyun <shim020924@gmail.com>
2025-01-02 12:45:03 +0900

    [Feat] Add Admin

--- client/src/pages/admin/AdminPage.tsx
+++ client/src/pages/admin/AdminPage.tsx
@@ -1,13 +1,8 @@
-// import usePostPortfolio from "@hooks/apis/usePostPortfolio";
-// import { dummy1, dummy2, dummy3, dummy4, dummy5 } from "./dummyPorfolio";
-
 const AdminPage = () => {
-  // const { postPortfolio } = usePostPortfolio();
-
   return (
     <div>
       <h1>어드민</h1>
-      <div>관리용 페이지입니다.</div>
+      <div>관리자용 페이지입니다.</div>
     </div>
   );
 };
